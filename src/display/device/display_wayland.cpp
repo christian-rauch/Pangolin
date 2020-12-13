2fd32f3253a2fe47a6211cc6cc2f09de72339e72
Christian Rauch <Christian.Rauch@ed.ac.uk>
2020-12-13 16:38:45 +0000

    remove EGL alpha channel

--- src/display/device/display_wayland.cpp
+++ src/display/device/display_wayland.cpp
@@ -379,7 +379,6 @@ struct WaylandDisplay {
         EGL_RED_SIZE        , 8,
         EGL_GREEN_SIZE      , 8,
         EGL_BLUE_SIZE       , 8,
-        EGL_ALPHA_SIZE      , 8,
         EGL_DEPTH_SIZE      , 24,
         EGL_STENCIL_SIZE    , 8,
         EGL_NONE
