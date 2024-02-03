c2e09d3884a90dc6417f5f31ffa6b9369278e3ac
Christian Rauch <Rauch.Christian@gmx.de>
2024-02-03 02:25:48 +0100

    [DBG] remove stuff from uvc media

--- components/pango_video/include/pangolin/video/drivers/uvc_mediafoundation.h
+++ components/pango_video/include/pangolin/video/drivers/uvc_mediafoundation.h
@@ -1,12 +1,7 @@
 #pragma once
 
 #include <pangolin/video/video_interface.h>
-
-struct IMFActivate;
-struct IMFMediaSource;
-struct IMFSourceReader;
-struct IBaseFilter;
-struct IKsControl;
+#include <cstdint>
 
 namespace pangolin
 {
@@ -16,78 +11,54 @@ class PANGOLIN_EXPORT UvcMediaFoundationVideo
     : public pangolin::VideoInterface, public pangolin::VideoUvcInterface, public pangolin::VideoPropertiesInterface
 {
   public:
-    UvcMediaFoundationVideo(int vendorId, int productId, const std::string& instanceId, size_t width, size_t height, int fps);
-    ~UvcMediaFoundationVideo();
+    UvcMediaFoundationVideo(int vendorId, int productId, const std::string& instanceId, size_t width, size_t height, int fps) {};
+    ~UvcMediaFoundationVideo() {};
 
     //! Implement VideoInput::Start()
-    void Start();
+    void Start() {};
 
     //! Implement VideoInput::Stop()
-    void Stop();
+    void Stop() {};
 
     //! Implement VideoInput::SizeBytes()
-    size_t SizeBytes() const;
+    size_t SizeBytes() const { return 0; };
 
     //! Implement VideoInput::Streams()
-    const std::vector<pangolin::StreamInfo>& Streams() const;
+    const std::vector<pangolin::StreamInfo>& Streams() const { return {}; };
 
     //! Implement VideoInput::GrabNext()
-    bool GrabNext(unsigned char* image, bool wait = true);
+    bool GrabNext(unsigned char* image, bool wait = true) { return true; };
 
     //! Implement VideoInput::GrabNewest()
-    bool GrabNewest(unsigned char* image, bool wait = true);
+    bool GrabNewest(unsigned char* image, bool wait = true) { return true; };
 
     //! Implement VideoUvcInterface::GetCtrl()
-    int IoCtrl(uint8_t unit, uint8_t ctrl, unsigned char* data, int len, pangolin::UvcRequestCode req_code);
+    int IoCtrl(uint8_t unit, uint8_t ctrl, unsigned char* data, int len, pangolin::UvcRequestCode req_code) { return 0; };
 
     //! Implement VideoUvcInterface::GetExposure()
-    bool GetExposure(int& exp_us);
+    bool GetExposure(int& exp_us) { return true; };
 
     //! Implement VideoUvcInterface::SetExposure()
-    bool SetExposure(int exp_us);
+    bool SetExposure(int exp_us) { return true; };
 
     //! Implement VideoUvcInterface::GetGain()
-    bool GetGain(float& gain);
+    bool GetGain(float& gain) { return true; };
 
     //! Implement VideoUvcInterface::SetGain()
-    bool SetGain(float gain);
+    bool SetGain(float gain) { return true; };
 
     //! Access JSON properties of device
-    const picojson::value& DeviceProperties() const;
+    const picojson::value& DeviceProperties() const { return {}; };
 
     //! Access JSON properties of most recently captured frame
-    const picojson::value& FrameProperties() const;
+    const picojson::value& FrameProperties() const { return {}; };
 
   protected:
-    bool FindDevice(int vendorId, int productId, const std::string& instanceId);
     void InitDevice(size_t width, size_t height, bool async);
-    void DeinitDevice();
-    void PopulateGainControls();
-
-    static bool DeviceMatches(const std::wstring& symLink, int vendorId, int productId, std::wstring& instanceId);
-    static bool SymLinkIDMatches(const std::wstring& symLink, const wchar_t* idStr, int id);
-
-    std::vector<pangolin::StreamInfo> streams;
-    size_t size_bytes;
 
     IMFMediaSource* mediaSource;
     AsyncSourceReader* asyncSourceReader;
-    IMFSourceReader* sourceReader;
-    IBaseFilter* baseFilter;
-    IKsControl* ksControl;
-    DWORD ksControlNodeId;
-    IAMCameraControl* camera_control;
-    IAMVideoProcAmp* video_control;
 
-    long gainCamMin;
-    long gainCamMax;
-    long gainCamDefault;
-
-    const float gainApiMin = 1.0f;
-    const float gainApiMax = 15.5f;
     int64_t expected_fps;
-
-    picojson::value device_properties;
-    picojson::value frame_properties;
 };
 }
